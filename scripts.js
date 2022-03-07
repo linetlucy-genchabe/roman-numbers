aee58cfc2f8e74e64ed844c061398a4c8092e1aa
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-07 10:27:53 +0300

    create js script and initialize var

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const romanNum = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+};
+
+const s = "MCMLXXXIX";
+
+// function romanToInt(s) {
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === "I" && s[i + 1] === "v") {
+//             accumulator += 4;
+//             i++;
+//         } else if (s[i] === "I" && s[i + 1] === "X") {
+//             accumulator += 9;
+//             i++;
+//         } else if (s[i] === "X" && s[i + 1] === "L") {
+//             accumulator += 40;
+//             i++;
+//         } else if (s[i] === "X" && s[i + 1] === "C") {
+//             accumulator += 90;
+//             i++;
+//         } else if (s[i] === "C" && s[i + 1] === "D") {
+//             accumulator += 400;
+//             i++;
+//         } else if (s[i] === "C" && s[i + 1] === "M") {
+//             accumulator += 900;
+//             i++;
+//         } else {
+//             accumulator += romanNum[s[i]];
+//         }
+//     }
+//     return accumulator;
+// }
